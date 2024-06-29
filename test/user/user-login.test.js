6c018cf59f57621cfc0ba925412b83e3250434cf
Ahmad Naufal <naufalopal002@gmail.com>
2024-06-29 10:08:58 +0700

    test: :white_check_mark: New test for get user and get user API is passed

---
+++ test/user/user-login.test.js
@@ -0,0 +1,69 @@
+import supertest from "supertest";
+import { web } from "../../src/application/web.js";
+import { logger } from "../../src/application/logging.js";
+import { createTestUser, removeTestUser } from "../test-util.js";
+
+describe('POST /api/users/login', function () {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can login', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "rahasia"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.token).toBeDefined();
+        expect(result.body.data.token).not.toBe("test");
+    });
+    
+    it('should reject login if username and password is empty or invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "",
+                password: ""
+            });
+        const message = result.text.match(/Error: .*?(?=<br>)/)[0]
+        logger.info(message);
+
+        expect(result.status).toBe(400);
+        expect(result.error).toBeDefined();
+    });
+    it('should reject login if username is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "salah",
+                password: "rahasia"
+            });
+        const message = result.text.match(/Error: .*?(?=<br>)/)[0]
+        logger.info(message);
+
+        expect(result.status).toBe(401);
+        expect(result.error).toBeDefined();
+    });
+    it('should reject login if password is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "salah"
+            });
+        const message = result.text.match(/Error: .*?(?=<br>)/)[0]
+        logger.info(message);
+
+        expect(result.status).toBe(401);
+        expect(result.error).toBeDefined();
+    });
+});
--- test/user/user-register.test.js
+++ test/user/user-register.test.js
@@ -1,8 +1,7 @@
 import supertest from "supertest";
-import { web } from "../src/application/web.js";
-import { logger } from "../src/application/logging.js";
-import { createTestUser, removeTestUser } from "./test-util.js";
-
+import { web } from "../../src/application/web.js";
+import { logger } from "../../src/application/logging.js";
+import { removeTestUser } from "../test-util.js";
 describe('POST /api/users', function () {
 
     afterEach(async () => {
@@ -18,7 +17,7 @@ describe('POST /api/users', function () {
                 name: 'test'
             });
 
-        // logger.info(result.body);
+        logger.info(result.body);
 
         expect(result.status).toBe(200);
         expect(result.body.data.username).toBe("test");
@@ -34,38 +33,12 @@ describe('POST /api/users', function () {
                 password: '',
                 name: ''
             });
+        const message = result.text.match(/Error: .*?(?=<br>)/)[0]
 
-        logger.info(result.body);
+        logger.info(message);
 
         expect(result.status).toBe(400);
-        expect(result.body.errors).toBeDefined;
+        expect(result.error).toBeDefined();
 
     });
-});
-
-describe('POST /api/users/login', function () {
-    beforeEach(async () => {
-        await createTestUser();
-    });
-
-    afterEach(async () => {
-        await removeTestUser();
-    });
-
-    it('should can login', async () => {
-        const result = await supertest(web)
-            .post('/api/users/login')
-            .send({
-                username: "test",
-                password: "rahasia"
-            });
-
-        logger.info(result.body)
-
-        expect(result.status).toBe(200);
-        expect(result.body.data.token).toBeDefined();
-        expect(result.body.data.token).not.toBe("test");
-    });
-
-
 });
